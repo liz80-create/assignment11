d849a4128f9a505d5f78f1130030e8c4917cba28
liz80-create <charaniyalizna@gmail.com>
2023-12-12 21:07:38 +0530

    first commit

---
+++ div.py
@@ -0,0 +1,9 @@
+def divide_numbers(dividend, divisor):
+    return dividend / divisor
+
+dividend = float(input("Enter the dividend: "))
+divisor = float(input("Enter the divisor: "))
+
+result = divide_numbers(dividend, divisor)
+
+print(f"The result of {dividend} divided by {divisor} is: {result}")
